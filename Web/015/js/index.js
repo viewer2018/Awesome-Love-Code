6266ab5bf0d5f4b879310e40314a93a43f104ed9
viewer20188 <31041794+viewer2018@users.noreply.github.com>
2024-05-18 23:27:23 +0800

    Update index.js

--- Web/015/js/index.js
+++ Web/015/js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||某某某|我们|在一起吧|❤|#rectangle|');
+      S.UI.simulate('|#countdown 3||麦兜麦兜|我们|在一起吧|永远|❤|#rectangle|');
     }
 
     S.Drawing.loop(function () {
